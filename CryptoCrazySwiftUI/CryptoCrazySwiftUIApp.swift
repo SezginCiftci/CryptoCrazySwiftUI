b135af50e02b1b39f6943026c3960cf2ba32349c
Sezgin Çiftçi <88513574+SezginCiftci@users.noreply.github.com>
2022-07-31 16:26:31 +0300

    Initial Commit

---
+++ CryptoCrazySwiftUI/CryptoCrazySwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  CryptoCrazySwiftUIApp.swift
+//  CryptoCrazySwiftUI
+//
+//  Created by Sezgin Çiftci on 31.07.2022.
+//
+
+import SwiftUI
+
+@main
+struct CryptoCrazySwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
